69b0785609523a0476a891a366a5a5e1ed509f98
Reuben <reubenharry@gmail.com>
2017-02-18 19:13:24 -0800

    utils linked

--- data_munge.py
+++ data_munge.py
@@ -3,6 +3,49 @@ from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 import numpy as np
 from keras.preprocessing import sequence
+# from keras.preprocessing.text import text_to_word_sequence
+from utils.preprocessing import preprocess_captions
+
+
+# def preprocess_captions(captions):
+#     caption_seqs = [text_to_word_sequence(c) for c in captions]
+#     unique = unique_words(caption_seqs)
+
+#     word_to_idx = {}
+#     idx_to_word = {}
+#     for i, word in enumerate(unique):
+#         # Start indices at 1 since 0 will represent padding
+#         word_to_idx[word] = i+1
+#         idx_to_word[i+1] = word
+
+#     partial_caps, next_words = partial_captions_and_next_words(caption_seqs, word_to_idx)
+
+#     return unique, word_to_idx, idx_to_word, partial_caps, next_words
+
+
+# def unique_words(caption_seqs):
+#     unique = set()
+#     for seq in caption_seqs:
+#         for word in seq:
+#             unique.add(word)
+
+#     return list(unique)
+
+
+# def partial_captions_and_next_words(caption_seqs, word_to_idx):
+#     max_caption_len = max([len(seq) for seq in caption_seqs])
+#     partial_caps = []
+#     next_words = []
+#     for seq in caption_seqs:
+#         for i, word in enumerate(seq[:-1]):
+#             partial_caps.append([word_to_idx[w] for w in seq[:i+1]])
+#             next_words.append(word_to_idx[seq[i+1]])
+
+#     # Pad sequences with 0's such that they all have length 'max_caption_len-1'. We subtract 1 b/c the
+#     # last word of a caption will never be included in any partial caption
+#     partial_caps = sequence.pad_sequences(partial_caps, maxlen=max_caption_len-1, padding='post')
+#     return partial_caps, next_words
+
 captions_path = '/Users/reuben/Downloads/annotations/captions_train2014.json'
 
 
@@ -16,51 +59,4 @@ number = captions['annotations'][0]['image_id']
 number = ('0000000000000'+str(number))[-12:]
 print(number)
 image_path = '/Users/reuben/Downloads/train2014/COCO_train2014_'+number+'.jpg'
-
-def preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(224, 224))
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)
-    return img
-
-# print(preprocess_image(image_path))
-max_caption_len = 16
-vocab_size = 10
-def preprocess_img_and_text(images, sentences):
-    images = [preprocess_image(image) for image in images]
-    images = np.asarray(images)
-
-
-    words = [s.split() for s in sentences]
-    unique = []
-    for word in words:
-        unique.extend(word)
-    unique = list(set(unique))
-    word_index = {}
-    index_word = {}
-    for i,word in enumerate(unique):
-        word_index[word] = i
-        index_word[i] = word
-
-    partial_captions = []
-    for s in sentences:
-        one = [word_index[txt] for txt in s.split()]
-        partial_captions.append(one)
-
-    partial_captions = sequence.pad_sequences(partial_captions, maxlen=max_caption_len,padding='post')
-    next_words = np.zeros((1,vocab_size))
-    for i,s in enumerate(sentences):
-        text = s.split()
-        x = [word_index[txt] for txt in text]
-        x = np.asarray(x)
-        next_words[i,x] = 1
-
-    return images, partial_captions, next_words
-
-print(preprocess_img_and_text([image_path],[sentence]))
-
-# first: takes first image and gives its caption
-
-
-# todo: returns a pickled list of numpy tensors of annotations, and a list of numpy tensors of images 
+print(preprocess_captions([str(sentence)]))
