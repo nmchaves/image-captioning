d10c0d3340e0478db248bbb8c076c402104aea57
Reuben <reubenharry@gmail.com>
2017-02-19 23:42:48 -0800

    pipeline

--- data_munge.py
+++ data_munge.py
@@ -2,10 +2,9 @@ import json
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 import numpy as np
+import cPickle as pickle
 from keras.preprocessing import sequence
 from utils.preprocessing import preprocess_captions,preprocess_image
-
-
 
 captions_path = '/Users/reuben/Downloads/annotations/captions_train2014.json'
 
@@ -15,24 +14,108 @@ captions = open(captions_path,'r')
 captions = json.loads(captions.read())
 
 
+# print(preprocess_captions(["hello what is your name","my name is blah"]))
 
-# print([x for x in captions])
-X = []
-y = []
-
-# make image dictionary first
 # image_dictionary = {}
-# for x in BLAH:
-# 	image_dictionary[x] = preprocess_image(x)
+# pickle.dump(image_dictionary,open("images","r+"))
+# new_dict = {}
+# for i,x in enumerate(captions['annotations']):
+# 	number = ('0000000000000'+str(x['image_id']))[-12:]
+# 	image_path = '/Users/reuben/Downloads/train2014/COCO_train2014_'+number+'.jpg'
+# 	new_dict[number] = preprocess_image(image_path)
+# 	print number,i
 
-for x in range(2):
-	sentence = (captions['annotations'][x]['caption'])
-	number = captions['annotations'][0]['image_id']
-	number = ('0000000000000'+str(number))[-12:]
+# 	if i%100 == 0:
+# 		image_dictionary = pickle.load(open("images","r+"))
+# 		print([x for x in image_dictionary])
+# 		image_dictionary.update(new_dict)
+# 		pickle.dump(new_dict,open("images","r+"))
+# 		new_dict = {}
+# 	if i == 300:
+# 		break
+
+# image_dictionary = pickle.load(open("images","r+"))
+# image_dictionary.update(new_dict)
+# pickle.dump(new_dict,open("images","r+"))
+# new_dict = {}
+# pickle.dump(image_dictionary,open("images","r+"))
+# image_dictionary = pickle.load(open("images","r+"))
+# print(len(image_dictionary))
+# print([x for x in captions])
+# X1 = []
+# X2 = []
+# y = []
+
+sentences = []
+for x in captions['annotations'][:2]:
+	sentences.append(str(x['caption']))
+
+
+unique, word_to_idx, idx_to_word, partial_caps, next_words = (preprocess_captions(sentences))
+
+vocab_size = len(word_to_idx)+1
+# print "NEXT WORDS",next_words
+
+new_next_words = []
+for x in next_words:
+	a = np.zeros(vocab_size)
+	a[x-1] = 1
+	new_next_words.append(a)
+next_words = new_next_words
+
+sentences = partial_caps
+images = []
+for x in captions['annotations'][:2]:
+	number = str(('0000000000000'+str(x['image_id']))[-12:])
 	image_path = '/Users/reuben/Downloads/train2014/COCO_train2014_'+number+'.jpg'
 	image = preprocess_image(image_path)
-	unique, word_to_idx, idx_to_word, partial_caps, next_words = (preprocess_captions([str(sentence)]))
-	X += [partial_caps,image]
-	y += next_words
+	image = image[0]
+	# print(len(x['caption'].split()))
+	# print(x['caption'])
+	for y in range(len(x['caption'].split())-1):
+		images.append(image)
 
-print(X,y)
+# print(sentences,images)
+# print(len(sentences),len(images),len(next_words))
+# print(sentences[0].shape)
+
+X1 = np.asarray(images)
+X2 = np.asarray(sentences)
+X = [X1,X2]
+# print(y)
+y = np.asarray(next_words)
+
+out = X,y,vocab_size,idx_to_word
+
+# print(X1.shape,X2.shape,y.shape,vocab_size)
+
+# print(captions['annotations'][:2])
+# print(word_to_idx,idx_to_word)
+pickle.dump(out, open( "savedoc", "r+" ) )
+
+# assert (len(sentences)==len(images))
+
+# #hmm, seems like i should do a single run of preprocess_captions
+# for x in range(2):
+# 	item = captions['annotations'][x]
+# 	sentence = (item['caption'])
+# 	# number = str(('0000000000000'+str(item['image_id']))[-12:])
+# 	# image = image_dictionary[number]
+# 	unique, word_to_idx, idx_to_word, partial_caps, next_words = (preprocess_captions([str(sentence)]))
+# 	vocab_size = len(word_to_idx)
+# 	assert (len(partial_caps) == len(next_words))
+# 	for partial_cap,next_word in zip(partial_caps,next_words):
+
+# 		# X1.append(image)
+# 		X2.append(partial_cap)
+# 		z = np.zeros(vocab_size)
+# 		y.append(next_word)
+
+# print(y.shape)
+	# X = image
+
+# # X = preprocess_image(image_path)
+
+
+# # print y
+
---
+++ keras_vgg_19/testcaptioning.py
@@ -0,0 +1,147 @@
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, \
+    Embedding, TimeDistributed, GRU, RepeatVector, Merge
+from keras.applications import VGG19
+from keras.preprocessing import image
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+import numpy as np
+from keras.preprocessing import sequence
+import pickle
+
+with open('savedoc', 'rb') as handle:
+    data = pickle.load(handle)
+
+# img = data[0][0][0]
+# cap = data[0][0][1][0].reshape(1,16)
+# vocab_size = data[2]
+# y = np.zeros((1,vocab_size))
+# y[0][data[1][0]] = 1
+
+# print(y.shape)
+
+# print(cap.shape)
+
+# # ideal
+X,y,vocab_size,idx_to_word = data  # ideally, Xand y should be tensors, right? well, X should be two tensors
+images = X[0] #shape (batch_size,224,224,3)
+captions = X[1] # (batch_size,16)
+next_words = y # vocab_size 
+
+# print(captions),"CAPTIONS"
+# image = images[0]
+# caption = captions[0]
+# next_word = next_words[0]
+
+# print(images.shape,captions.shape)
+
+# vocab_size = 7
+
+def preprocess_image(img_path):
+    img = image.load_img(img_path, target_size=(224, 224))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = preprocess_input(img)
+    return img
+
+def preprocess_img_and_text(images, sentences):
+    images = [preprocess_image(image) for image in images]
+    images = np.asarray(images)
+
+
+    words = [s.split() for s in sentences]
+    unique = []
+    for word in words:
+        unique.extend(word)
+    unique = list(set(unique))
+    word_index = {}
+    index_word = {}
+    for i,word in enumerate(unique):
+        word_index[word] = i
+        index_word[i] = word
+
+    partial_captions = []
+    for s in sentences:
+        one = [word_index[txt] for txt in s.split()]
+        partial_captions.append(one)
+
+    partial_captions = sequence.pad_sequences(partial_captions, maxlen=max_caption_len,padding='post')
+    next_words = np.zeros((1,vocab_size))
+    for i,s in enumerate(sentences):
+        text = s.split()
+        x = [word_index[txt] for txt in text]
+        x = np.asarray(x)
+        next_words[i,x] = 1
+
+    return images, partial_captions, next_words
+
+
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+if __name__ == '__main__':
+    max_caption_len = 10
+    vocab_size = vocab_size #10000
+
+    initial_model = VGG19(weights="imagenet", include_top=True)
+    flat = initial_model.get_layer('flatten').output
+    flat_repeated = RepeatVector(max_caption_len)(flat)
+    image_model = Model(initial_model.inputs, flat_repeated)
+
+    # next, let's define a RNN model that encodes sequences of words
+    # into sequences of 128-dimensional word vectors.
+    language_model = Sequential()
+    language_model.add(Embedding(vocab_size, 256, input_length=max_caption_len))
+    language_model.add(GRU(output_dim=128, return_sequences=True))
+    language_model.add(TimeDistributed(Dense(128)))
+
+    # let's repeat the image vector to turn it into a sequence.
+    #image_model.add(RepeatVector(max_caption_len))
+
+    # the output of both models will be tensors of shape (samples, max_caption_len, 128).
+    # let's concatenate these 2 vector sequences.
+    model = Sequential()
+    model.add(Merge([image_model, language_model], mode='concat', concat_axis=-1))
+    #model.add(Merge([image_model, language_model], mode='concat', concat_axis=-1))
+    # let's encode this vector sequence into a single vector
+    model.add(GRU(256, return_sequences=False))
+    # which will be used to compute a probability
+    # distribution over what the next word in the caption should be!
+    model.add(Dense(vocab_size))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+    # sentences = ["START A cat with white fur END"]
+    # images_path = ['cat.jpg']
+    # images_2, partial_captions, next_words = preprocess_img_and_text(images_path, sentences)
+
+    # image = preprocess_image('cat.jpg')
+
+    # print "next",next_words.shape
+    # print "shape partial_captions", partial_captions.shape
+    # print "partial captions 1",partial_captions[0]
+    # print image.shape
+    # print "shape of captions",captions.shape
+
+    # "images" is a numpy float array of shape (nb_samples, nb_channels=3, width, height).
+    # "captions" is a numpy integer array of shape (nb_samples, max_caption_len)
+    # containing word index sequences representing partial captions.
+    # "next_words" is a numpy float array of shape (nb_samples, vocab_size)
+    # containing a categorical encoding (0s and 1s) of the next word in the corresponding
+    # partial caption.
+    # model.fit([np.zeros((17,224,224,3)),np.zeros((17,10))], np.zeros((17,15)), batch_size=2, nb_epoch=5)
+    # print captions.shape
+
+    captions = np.zeros((17,10))
+    captions[0][0] = 10
+    # model.fit([X[0],captions], y, batch_size=2, nb_epoch=5)
+    result = model.predict([X[0][0].reshape([1,224,224,3]),X[1][0].reshape([1,10]) ])
+    # print result   
+    out = sample(result[0])
+    print(idx_to_word[out])
