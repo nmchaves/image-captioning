a5e4012c2c21e5f427c2579a8e4613579a990d02
Ubuntu <cs224n@cs224n-gpu2.syqc5mppprfuhoziajkfx13ich.bx.internal.cloudapp.net>
2017-03-20 07:38:55 +0000

    prep

--- keras_vgg_19/cnn_preprocessing.py
+++ keras_vgg_19/cnn_preprocessing.py
@@ -56,8 +56,8 @@ def predict_image(file_id,bbox=[]):
 	# f.write(output)
 	storage_dir = '/extra/'
 	np.save(file=storage_dir+'processed/'+number, arr=output)
-    np.save(file=storage_dir+'processed_flatten/'+number, arr=output2)
-    np.save(file=storage_dir+'processed_predictions/'+number, arr=output3)
+    	np.save(file=storage_dir+'processed_flatten/'+number, arr=output2)
+    	np.save(file=storage_dir+'processed_predictions/'+number, arr=output3)
 	'''
 	np.save(file=datasetDir+'processed/'+number, arr=output)
         np.save(file=datasetDir+'processed_flatten/'+number, arr=output2)
@@ -71,16 +71,16 @@ def predict_image(file_id,bbox=[]):
 		region = original.crop((int(bbox[0]),int(bbox[1]),int(bbox[0])+int(bbox[2]),int(bbox[1])+int(bbox[3])))
 		region = region.resize((224,224))
 		region = image.img_to_array(region)
-    	region = np.expand_dims(region, axis=0)
-    	region = preprocess_input(region)
+    		region = np.expand_dims(region, axis=0)
+    		region = preprocess_input(region)
 
-    	reg_output = model.predict(region)
-    	reg_output2 = model2.predict(region)
-    	reg_output3 = model3.predict(region)
+    		reg_output = model.predict(region)
+    		reg_output2 = model2.predict(region)
+    		reg_output3 = model3.predict(region)
 
-    	np.save(file=storage_dir+'processed/'+number+'_b', arr=output)
-		np.save(file=storage_dir+'processed_flatten/'+number+'_b', arr=output2)
-		np.save(file=storage_dir+'processed_predictions/'+number+'_b', arr=output3)
+    		np.save(file=storage_dir+'processed_b/'+number, arr=reg_output)
+		np.save(file=storage_dir+'processed_flatten_b/'+number, arr=reg_output2)
+		np.save(file=storage_dir+'processed_predictions_b/'+number, arr=reg_output3)
 
 		return output,output2,output3,reg_output,reg_output2,reg_output3
 	# print(file_id)
